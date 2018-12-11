27e971dafd204eecf2df1e620afc9a7060511b5f
Arthur PREVOST <artprevo@e3r7p2.42.fr>
2018-12-11 18:07:57 +0100

    WIP

--- srcs/ft_fill.c
+++ srcs/ft_fill.c
@@ -6,12 +6,31 @@
 /*   By: artprevo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/09 20:03:13 by artprevo          #+#    #+#             */
-/*   Updated: 2018/12/09 20:55:59 by artprevo         ###   ########.fr       */
+/*   Updated: 2018/12/11 18:07:13 by artprevo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
+char	*ft_clean(char *tab)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	return (tab);
+}
+
+static int	ft_line(char *tab, char *content, int i)
+{
+	while (content[i] != '\n')
+	{
+		tab[i] = content[i];
+		i++;
+	}
+	return (i);
+}
 char	*ft_fill(t_fill **begin)
 {
 	t_fill	*current;
